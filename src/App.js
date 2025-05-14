0d7fa75b56b653e9b89cf2260383b3a0eb50ce26
Sam <samellis1403@gmail.com>
2025-05-13 20:45:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,63 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import { auth } from "./firebase/init";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "firebase/auth";
+
 function App() {
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    setLoading(false);
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({});
+      }
+    });
+  }, []);
   function register() {
     console.log("register");
-    createUserWithEmailAndPassword(auth, "email@email.com", "test123").then(
-      (user) => {
-        console.log9(user);
-      }
-    )
-    .catch((error) => {
-      console.log(error);
-    });
-  }
-
-  function login() {
-    console.log("login");
-    signInWithEmailAndPassword(auth, 'Email', 'password')
+    createUserWithEmailAndPassword(auth, "email@email.com", "test123")
       .then((user) => {
-        // Signed in
-        console .log(user);
-        // ...
-      }
-      )
+        console.log9(user);
+      })
       .catch((error) => {
         console.log(error);
       });
   }
 
+  function login() {
+    console.log("login");
+    signInWithEmailAndPassword(auth, "email@email.com", "password")
+      .then(({ user }) => {
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function logout() {
+    signOut(auth);
+    setUser({});
+    console.log("logout");
+  }
+
   return (
     <div className="App">
-      <button conClick={register}>Register</button>
-      <button conClick={login}>Login</button>
+      <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Log out</button>
+      {loading ? 'loading...' : user.email}
     </div>
   );
 }
